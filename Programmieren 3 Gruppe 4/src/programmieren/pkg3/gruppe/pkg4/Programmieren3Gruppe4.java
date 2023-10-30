c82ae1e86335b70a215e974a2c80d3768cdb5987
LinuBau <linus.baun@student.jade-hs.de>
2023-10-30 10:50:57 +0100

    Hello World

--- Programmieren 3 Gruppe 4/src/programmieren/pkg3/gruppe/pkg4/Programmieren3Gruppe4.java
+++ Programmieren 3 Gruppe 4/src/programmieren/pkg3/gruppe/pkg4/Programmieren3Gruppe4.java
@@ -5,6 +5,7 @@ public class Programmieren3Gruppe4 {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
