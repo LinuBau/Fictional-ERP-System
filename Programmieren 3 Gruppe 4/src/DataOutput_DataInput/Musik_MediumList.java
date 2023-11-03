1e4fd085174d8d15647cea60de0d8793ee221c92
LinuBau <linus.baun@student.jade-hs.de>
2023-11-03 09:32:40 +0100

    Input Output for Array List

---
+++ Programmieren 3 Gruppe 4/src/DataOutput_DataInput/Musik_MediumList.java
@@ -0,0 +1,13 @@
+
+package DataOutput_DataInput;
+
+import GeschaftsObejekt.Musik_Medium;
+import java.util.ArrayList;
+
+public class Musik_MediumList {
+     protected ArrayList<Musik_Medium> list = new ArrayList<>();
+
+    public void add(Musik_Medium mm){
+        list.add(mm);
+    }
+}
