0493ce8ac76ac4ddf1848a9beaf8ee07091cb63e
Xanx4s <148896991+Xanx4s@users.noreply.github.com>
2023-12-21 14:08:00 +0100

    add Changelog detail
    add Changelog detail

--- Programmieren 3 Gruppe 4/src/Traversierung/ChangeLogEntry.java
+++ Programmieren 3 Gruppe 4/src/Traversierung/ChangeLogEntry.java
@@ -5,3 +5,59 @@ import GeschaftsObejekt.Musik;
 import java.time.LocalDateTime;
 
 
+public class ChangeLogEntry {
+    private LocalDateTime timestamp;
+    private String action;
+    private Musik originalState;
+    private Musik newState;
+
+    public ChangeLogEntry(String action, Musik originalState, Musik newState) {
+        this.timestamp = LocalDateTime.now(); 
+        this.action = action;
+        this.originalState = originalState; 
+        this.newState = newState; 
+    }
+    // Getter
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public Musik getOriginalState() {
+        return originalState;
+    }
+
+    public Musik getNewState() {
+        return newState;
+    }
+
+    // Setter
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public void setOriginalState(Musik originalState) {
+        this.originalState = originalState;
+    }
+
+    public void setNewState(Musik newState) {
+        this.newState = newState;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangeLogEntry{" +
+                "timestamp=" + timestamp +
+                ", action='" + action + '\'' +
+                ", originalState=" + (originalState != null ? originalState.toString() : "null") +
+                ", newState=" + (newState != null ? newState.toString() : "null") +
+                '}';
+    }
+}
