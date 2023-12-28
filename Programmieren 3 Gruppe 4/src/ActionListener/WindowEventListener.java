719893211d4d69d9780af0baca7582f343caa04d
Fluteorchestra <145915127+Fluteorchestra@users.noreply.github.com>
2023-12-28 17:02:25 +0100

    Update WindowEventListener.java

--- Programmieren 3 Gruppe 4/src/ActionListener/WindowEventListener.java
+++ Programmieren 3 Gruppe 4/src/ActionListener/WindowEventListener.java
@@ -1,16 +1,21 @@
 
 package ActionListener;
 
+import Actions.FileSaveAction;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.IOException;
-
 import App_GUI.Gui;
 import GeschaftsObejekt.MusikList;
+import SaveData_ReadData.MusikCsvListDAO;
 import SaveData_ReadData.MusikListDAO;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 
-public class WindowEventListener implements WindowListener{
+public class WindowEventListener implements WindowListener {
     private Gui parent;
     private MusikList musikList;
     public WindowEventListener(Gui p){
@@ -26,7 +31,33 @@ public class WindowEventListener implements WindowListener{
     }
 
     @Override
-    public void windowClosing(WindowEvent e)    {
+    public void windowClosing(WindowEvent e) {
+        
+        String[] options = {"Speichern", "Speichern als", "Beenden"};      
+        int x = JOptionPane.showOptionDialog(null, "Möchten sie ihren Frortschritt speichern?",
+                "Click a button", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        
+        if (x==1){
+            JFileChooser chooser = new JFileChooser();
+            chooser.setAcceptAllFileFilterUsed(false);
+            chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+            int returnval = chooser.showSaveDialog(parent);
+            if (returnval == chooser.APPROVE_OPTION) {
+                String path = chooser.getSelectedFile().getAbsolutePath();
+                if (!path.endsWith(".csv")) {
+                    path = path + ".csv";
+                }
+                MusikCsvListDAO mmd = new MusikCsvListDAO(path, true);
+                try {
+                    mmd.write(parent.getMusikMap().getMusikList());
+                    mmd.close();
+                } 
+                catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        
         MusikListDAO mld = new MusikListDAO("setup.data", true);
         try {
             mld.write(musikList);
@@ -34,7 +65,8 @@ public class WindowEventListener implements WindowListener{
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-        System.out.println("Fenster schließen");
+        
+        System.out.println("Fenster schließen");        
         System.exit(0);    
     }
 
