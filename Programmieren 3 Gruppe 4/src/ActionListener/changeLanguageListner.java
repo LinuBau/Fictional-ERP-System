f64cbcac0b95569b66f87fe7e2519679d31d2c8f
Fluteorchestra <145915127+Fluteorchestra@users.noreply.github.com>
2024-01-14 12:31:51 +0100

    Update changeLanguageListner.java

--- Programmieren 3 Gruppe 4/src/ActionListener/changeLanguageListner.java
+++ Programmieren 3 Gruppe 4/src/ActionListener/changeLanguageListner.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Locale;
 
+import javax.swing.JFrame;
+
 import App_GUI.Gui;
 
 public class changeLanguageListner implements ActionListener {
@@ -21,7 +23,7 @@ public class changeLanguageListner implements ActionListener {
         int index = parent.getProfilList().getIndexofLogin();
         Gui mainWindow = new Gui(parent.getProfilList().get(index).getIsmitarbeiter(),parent.getProfilList(),parent.getMusikMap().getMusikList(),new Locale(language));
         mainWindow.setTitle(mainWindow.getL10NText("mk"));
-        mainWindow.setSize(1000, 500);
+        mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
         mainWindow.setLocationRelativeTo(null);
         parent.setVisible(false);
         mainWindow.setVisible(true);
